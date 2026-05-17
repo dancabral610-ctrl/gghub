442dc2c647d0844f09fbb5c02e596bac4a43c8dc
GG Hub <dancabral610@gmail.com>
2026-05-17 09:50:16 -0300

    Delete api/load.js

--- api/load.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-    const script = `
-loadstring(game:HttpGet("https://gist.githubusercontent.com/dancabral610-ctrl/6ac8ae154ea0bfb940f46d46f842ff60/raw/24761fee1eec0a6e0f73c5aba4079ff3d1e9e5a2/gistfile1.txt"))()
-`
-
-    res.setHeader("Content-Type", "text/plain")
-    res.status(200).send(script)
-}
